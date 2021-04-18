cf3bf52fe93e9ca3d8140bdb0c661b02cfc77998
Abzal <abzal281088@gmail.com>
2021-04-18 21:26:34 +0600

    Задание 1

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+
+  public static void main(String[] args) {
+
+    Pointdata P = new Pointdata(2, 3, 1, 4);
+    System.out.println("расстояние точек =" + distance(P));
+   }
+
+  public static double distance(Pointdata P) {
+    return Math.sqrt((P.p3 - P.p1) * (P.p3 - P.p1) + (P.p4 - P.p2) * (P.p4 - P.p2));
+  }
+}
